ff0c31d6f5bd3de0cc75440b31797292ca7002f8
Inaki Villar <inaki.seri@gmail.com>
2026-01-06 17:09:50 +0100

    Adding new element

--- repository/sync/src/test/kotlin/com/awesomeapp/sync/Activity15_29Test.kt
+++ repository/sync/src/test/kotlin/com/awesomeapp/sync/Activity15_29Test.kt
@@ -20,6 +20,7 @@ class Activity15_29Test {
         @Test
     fun `placeholder - activity should be tested in androidTest`() {
         // Activities should be tested with Instrumented tests (androidTest)
+        Thread.sleep(30_000)
         assertTrue(true)
     }
 
